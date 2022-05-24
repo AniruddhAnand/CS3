589bffa11399396d5c1ac26a889166bee57feeef
AniruddhAnand <65101767+AniruddhAnand@users.noreply.github.com>
2022-05-24 14:34:02 -0500

    Update

--- USACO3/src/CountingLiars.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class CountingLiars {
-    public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        int pos = console.nextInt();
-    }
-}
